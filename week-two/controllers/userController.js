7d9ef7314532d2ad774d84b13aea49c2d13ee2eb
darjapo <darja.polevaja@metropolia.fi>
2022-11-02 10:47:21 +0200

    password of users is not shown on the page

--- week-two/controllers/userController.js
+++ week-two/controllers/userController.js
@@ -5,6 +5,11 @@ const userModel = require('../models/userModel');
 const users = userModel.users;
 
 const getUsers = (req, res) => {
+    // removes the passwords property of users from showing
+    users.map(user => {
+        delete user.password;
+        return user;
+    });
     res.json(users);
 };
 
@@ -12,6 +17,7 @@ const getUser = (req, res) => {
     // chooses one object of an array with matching Id
     const user = users.filter(user => req.params.userId == user.id)[0];
     if (user) {
+        delete user.password;
         res.json(user);
     } else {
         res.sendStatus(404);
