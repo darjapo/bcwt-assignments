6bf72eee35b2c83dd04817ed47bcfaaedf2e3f7b
darjapo <darja.polevaja@metropolia.fi>
2022-11-22 15:06:56 +0200

    session task done

--- state-manager-example/app.js
+++ state-manager-example/app.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 const port = 3000;
 
@@ -17,6 +18,12 @@ app.set('view engine', 'pug');
 app.use(cookieParser());
 app.use(express.json()); //for parsing application/json
 app.use(express.urlencoded({ extended: true })); //for form data
+app.use(session({
+  secret: 'DGk34TGh!fgkn34!',
+  saveUninitialized: false,
+  resave: true,
+  cookie: {maxAge: 60000}
+}));
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -27,7 +34,7 @@ app.get('/form', (req, res) => {
 });
 
 app.get('/secret', (req, res) => {
-  if (loggedIn) {
+  if (req.session.loggedIn) {
     res.render('secret');
   } else {
     res.redirect('/form');
@@ -36,10 +43,16 @@ app.get('/secret', (req, res) => {
 
 app.post('/login', (req, res) => {
   // check for username/password match
-  if (req.body.username == dummyUser.username && req.body.password == dummyUser.password) {
-    loggedIn = true;
+  if (req.body.username === dummyUser.username && req.body.password === dummyUser.password) {
+    req.session.loggedIn = true;
   };
   res.redirect('/secret');
+});
+
+app.get('/logout', (req, res) => {
+  req.session.loggedIn = false;
+  //res.clearCookie('connect.sid'); //cookie for the session, depends on the case, sometimes might be needed
+  res.redirect('/');
 });
 
 app.get('/getCookie', (req, res) => {
