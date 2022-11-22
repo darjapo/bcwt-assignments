9fb0176f0805ddb3b2438614ec879664c8f96067
darjapo <darja.polevaja@metropolia.fi>
2022-11-22 14:20:40 +0200

    user aut and redirection

--- state-manager-example/app.js
+++ state-manager-example/app.js
@@ -4,12 +4,42 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
+const dummyUser = {
+  username: 'foo',
+  password: 'bar'
+};
+
+// don't use global variables
+let loggedIn = false;
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(cookieParser());
+app.use(express.json()); //for parsing application/json
+app.use(express.urlencoded({ extended: true })); //for form data
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.get('/secret', (req, res) => {
+  if (loggedIn) {
+    res.render('secret');
+  } else {
+    res.redirect('/form');
+  }
+});
+
+app.post('/login', (req, res) => {
+  // check for username/password match
+  if (req.body.username == dummyUser.username && req.body.password == dummyUser.password) {
+    loggedIn = true;
+  };
+  res.redirect('/secret');
 });
 
 app.get('/getCookie', (req, res) => {
